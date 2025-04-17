8e999c651dad98a7b4fbf80b5bff8bdc96e02504
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2025-04-17 09:58:46 -0300

    add files brief

--- src/FuelDataLayer.h
+++ src/FuelDataLayer.h
@@ -1,6 +1,6 @@
 /**
  * @file FuelDataLayer.h
- * @brief TODO: add a brief description.
+ * @brief Defines the FuelDataLayer template class for fuel parameters stored in NetCDF format.
  * @copyright Copyright (C) 2025 ForeFire, Fire Team, SPE, CNRS/Universita di Corsica.
  * @license This program is free software; See LICENSE file for details. (See LICENSE file).
  * @author Jean‑Baptiste Filippi — 2025
