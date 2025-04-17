1154826caabe9aa7528fea5fb03e3e5160e01440
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2025-04-17 09:27:15 -0300

    simple briefs

--- src/FarsitePropagationModel.cpp
+++ src/FarsitePropagationModel.cpp
@@ -1,6 +1,6 @@
 /**
  * @file FarsitePropagationModel.cpp
- * @brief TODO: add a brief description.
+ * @brief Farsite Propagation Model
  * @copyright Copyright (C) 2025 ForeFire, Fire Team, SPE, CNRS/Universita di Corsica.
  * @license This program is free software; See LICENSE file for details. (See LICENSE file).
  * @author Jean‑Baptiste Filippi — 2025
--- src/TimeGradientDataLayer.h
+++ src/TimeGradientDataLayer.h
@@ -1,6 +1,6 @@
 /**
  * @file TimeGradientDataLayer.h
- * @brief TODO: add a brief description.
+ * @brief Implicit data layer for having a view on the gradient of a given data layer
  * @copyright Copyright (C) 2025 ForeFire, Fire Team, SPE, CNRS/Universita di Corsica.
  * @license This program is free software; See LICENSE file for details. (See LICENSE file).
  * @author Jean‑Baptiste Filippi — 2025
